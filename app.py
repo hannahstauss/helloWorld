2c718725b7146f74ff6553c39f7cd0de8a359087
Hannah Stauss <hstauss@terpmail.umd.edu>
2023-03-27 22:25:08 -0400

    Added new html pages such as base.html (a parent html page), contact.html (a contact information form), favorite-course.html (a page that has URL input), and index.html for bootstrap. Contact and favorite-course used child pages for design purposes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,5 +15,17 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
+@app.route('/favorite-course')
+def favorite_course():
+    print('You entered your favorite course as: ' + request.args.get('subject') + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
